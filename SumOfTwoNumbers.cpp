dbc9bc26d52504737cc538e7ff6f40e91eeeb9dd
Devendra Singh Thakur <devendrasinghthakur98@gmail.com>
2023-07-31 14:50:38 +0530

    First commit here

---
+++ SumOfTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1, var2, result;
+    cout << "Enter 2 numbers";
+    cin >> var1 >> var2;
+
+    cout << " Sum of 2 numbers = " << result;
+    return 0;
+}
