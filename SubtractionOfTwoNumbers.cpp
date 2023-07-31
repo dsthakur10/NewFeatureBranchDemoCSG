16430172b8638e609a6e371217d57933a978f842
Devendra Singh Thakur <devendrasinghthakur98@gmail.com>
2023-07-31 15:04:02 +0530

    New feature in Feature_Branch

---
+++ SubtractionOfTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1, var2, result;
+    cout << "Enter 2 numbers";
+    cin >> var1 >> var2;
+
+    cout << " Var1 - Var2 = " << result;
+    return 0;
+}
